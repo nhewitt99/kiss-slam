8da02b1ee98db49cfeb57425513c33a993b9f824
nhewitt99 <mail@nhewitt.me>
2025-04-28 19:59:53 -0400

    Generate and install config from pydantic

--- kiss_slam/ros/src/kiss_slam_ros/setup.py
+++ kiss_slam/ros/src/kiss_slam_ros/setup.py
@@ -22,7 +22,11 @@
 # SOFTWARE.
 from setuptools import find_packages, setup
 
+from kiss_slam.config.config import write_config
+
 package_name = "kiss_slam_ros"
+
+write_config(filename="config/default.yaml")
 
 setup(
     name=package_name,
@@ -31,6 +35,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/config", ["config/default.yaml"]),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
